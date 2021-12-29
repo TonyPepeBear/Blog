10f962ede0d34b9abd8aae9045927d82b3d94aac
TonyPepeBear <41405045+TonyPepeBear@users.noreply.github.com>
2021-12-29 17:29:10 +0000

    init npm and taiwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['layouts/**/*.html'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

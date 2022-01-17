932b19065a4bc865762d88b67a42892ab5a7682e
TonyPepeBear <TonyPepeBear@users.noreply.github.com>
2022-01-17 15:05:49 +0000

    deploy: 296a9ebf83a091ee0bebc19a51c065f3d2ce5eaa

---
+++ js/search.js
@@ -0,0 +1,37 @@
+const search = instantsearch({
+  indexName: 'hugo_blog',
+  searchClient: instantMeiliSearch(
+    'https://search.tonypepe.com',
+    '6xWF8OOP6f0a7f4c0eaddff20b31a8f4e3dadbd3ccf40f809da73ab625f6ddc6c7dc9d72'
+  ),
+})
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+    showSubmit: false,
+    showLoadingIndicator: false,
+    showReset: false,
+    autofocus: true,
+    placeholder: "Search",
+  }),
+  instantsearch.widgets.configure({
+    hitsPerPage: 6,
+  }),
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      empty: 'No results',
+      item: `
+          <div class="hit-name">
+            <a href='{{ uri }}'>
+              <h2>{{ title }}</h2>
+              <p>{{ summary }}</p>
+            </a>
+          </div>
+        `,
+    },
+  }),
+])
+
+search.start()

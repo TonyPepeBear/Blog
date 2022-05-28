a27790aea48878f20df4aef49fce8de803672f17
TonyPepeBear <tonybear55665566@gmail.com>
2022-05-28 19:49:18 +0000

    Update

---
+++ src/components/aritcle.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import "../styles/article.css";
+import { Icon } from "@iconify/react";
+
+export default function Article(props: Props) {
+  const { node } = props;
+  const { title, image, date, tags } = node.frontmatter;
+  const outputDate = new Date(date).toLocaleDateString();
+  return (
+    <div className="bg-white w-full rounded-xl shadow-md break-words">
+      <img
+        src={image}
+        alt={title}
+        className="w-full rounded-t-xl h-72 lg:h-96 object-cover"
+      />
+      <div className="p-5">
+        <h1 className="text-4xl font-medium">{title}</h1>
+        <div className="flex flex-row flex-wrap gap-x-4 gap-y-1 py-4">
+          {/* Date */}
+          <div className="text-xl flex gap-2 items-center text-gray-700 font-medium">
+            <Icon icon="mdi:calendar-month" />
+            <time dateTime={date}>{outputDate}</time>
+          </div>
+          {/* Issue */}
+          <a
+            href="http://github.com/tonypepebear/hugoblog/issues"
+            target="_blank"
+          >
+            <div className="text-xl flex gap-2 items-center text-gray-700 font-medium">
+              <Icon icon="entypo:info-with-circle" />
+              <span>Issue</span>
+            </div>
+          </a>
+        </div>
+        {/* 分隔符 */}
+        <div>
+          <div className="h-4" />
+          <hr />
+          <div className="h-4" />
+        </div>
+        {/* Article */}
+        <div
+          className="article-content"
+          dangerouslySetInnerHTML={{ __html: node.html }}
+        />
+        {/* Tages */}
+        {tags && (
+          <div className="flex py-3 flex-wrap gap-x-3 gap-y-4 items-center">
+            <Icon icon="mdi:tag" />
+            {tags.map((tag) => (
+              <span className="px-3 py-1 text-sm bg-gray-200 rounded-full">
+                {tag}
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+interface Props {
+  node: {
+    html: any;
+    excerpt: string;
+    rawMarkdownBody: string;
+    frontmatter: {
+      title: string;
+      date: string;
+      draft: boolean;
+      tags: string[];
+      image: string;
+      description?: string;
+    };
+  };
+}
--- src/templates/article-template.tsx
+++ src/templates/article-template.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { graphql } from "gatsby";
 import MainLayout from "../components/layouts/main-layouts";
-import { marked } from "marked";
+import Article from "../components/aritcle";
 
 export default function ArticleTemplate(props: Props) {
   const { data } = props;
@@ -9,25 +9,9 @@ export default function ArticleTemplate(props: Props) {
     <MainLayout>
       <div>
         <div className="flex flex-col w-full gap-14">
-          {data.allMarkdownRemark.edges.map(({ node }) => {
-            const { title, image } = node.frontmatter;
-            return (
-              <div className="bg-white w-full rounded-xl shadow-md break-words">
-                <img
-                  src={image}
-                  alt={title}
-                  className="w-full rounded-t-xl h-96 object-cover"
-                />
-                <div className="p-5">
-                  <h1 className="text-4xl">{title}</h1>
-                  <div
-                    className="main-article"
-                    dangerouslySetInnerHTML={{ __html: node.html }}
-                  />
-                </div>
-              </div>
-            );
-          })}
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <Article node={node} />
+          ))}
         </div>
       </div>
     </MainLayout>

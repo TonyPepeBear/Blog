ae8ea2cf1207af0d8ed5d22cf64eb5bbb2eb61dc
TonyPepeBear <tonybear55665566@gmail.com>
2022-05-31 03:45:41 +0800

    Update

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,53 +1,18 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import MainLayout from "../components/layouts/main-layouts";
+import ReadMoreButton from "../components/read-more-button";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <MainLayout>
+      <div className="flex flex-col items-center gap-6">
+        <img
+          className="w-full rounded-full"
+          src="https://imagedelivery.net/cdkaXPuFls5qlrh3GM4hfA/139d267d-742f-4beb-e0f2-2a7f2aa82a00/public"
+        />
+        <ReadMoreButton href="/" title="Go Home" />
+      </div>
+    </MainLayout>
   );
 };
 

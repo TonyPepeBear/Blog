33fcbb56d1dc9feb01152a9e0e9c30b7c7cfbc62
TonyPepe <41405045+TonyPepeBear@users.noreply.github.com>
2023-07-24 11:22:10 +0800

    Update

--- src/components/layouts/main-layouts.tsx
+++ src/components/layouts/main-layouts.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { Helmet } from "react-helmet";
 import Footer from "../footer";
 import Navbar from "../navbar";
+import { Script } from "gatsby"
 
 interface Props {
   children: JSX.Element;
@@ -12,6 +13,7 @@ interface Props {
 export default function MainLayout({ children }: Props) {
   return (
     <div className="font-serif">
+      <Script src="https://umami.tonypepe.com/script.js" data-website-id="a2f8b5b9-1035-4981-a435-5505fa28d033" />
       <div className="bg-gray-400 flex flex-col lg:flex-row">
         <Navbar />
         <main className="px-2 lg:px-10 flex-grow w-full min-w-0">
